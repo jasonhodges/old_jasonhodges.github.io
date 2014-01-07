813826ced1df8948435c3fabd598afbb9852854d
Jason Hodges <jason@jasonhodges.me>
2014-01-06 23:42:03 -0500

    uncommented

--- js/main.js
+++ js/main.js
@@ -6,24 +6,24 @@
 
 
 //pygments be broken, look for code highlight blocks
-// (function($, undefined) {
-// 	var fauxHighlight = {
-// 		init : function() {
-// 			fauxHighlight.$code = $(".t-copy pre code[class]");
-// 			fauxHighlight.$codeBlock = fauxHighlight.$code.closest("div");
+(function($, undefined) {
+	var fauxHighlight = {
+		init : function() {
+			fauxHighlight.$code = $(".t-copy pre code[class]");
+			fauxHighlight.$codeBlock = fauxHighlight.$code.closest("div");
 
-// 			fauxHighlight.attachedInfo();
-// 		},
+			fauxHighlight.attachedInfo();
+		},
 
-// 		attachedInfo : function(){
-// 			fauxHighlight.$codeBlock.each(function(index){
-// 				var $current = $(this);
-// 					currentLanguage = fauxHighlight.$code.eq(index).attr("class");
+		attachedInfo : function(){
+			fauxHighlight.$codeBlock.each(function(index){
+				var $current = $(this);
+					currentLanguage = fauxHighlight.$code.eq(index).attr("class");
 				
-// 				$current.addClass("fauxHighlight").attr("data-lang", currentLanguage);
-// 			});
-// 		}
-// 	}
+				$current.addClass("fauxHighlight").attr("data-lang", currentLanguage);
+			});
+		}
+	}
 
-// 	fauxHighlight.init();
-// })(jQuery);
+	fauxHighlight.init();
+})(jQuery);
